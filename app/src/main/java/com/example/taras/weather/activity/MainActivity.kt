1ef64b1cebd3599e6f430cc5a0c480b1f599a717
Taras <sagan0993@gmail.com>
2018-10-28 13:16:58 +0200

    Initial commit

---
+++ app/src/main/java/com/example/taras/weather/activity/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.taras.weather.activity
+
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
+import com.example.taras.weather.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
